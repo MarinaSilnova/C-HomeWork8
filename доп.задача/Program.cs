789e1d1d3a6dfce15e89eab540aeef4486187653
Marina Silnova <marusia421@mail.ru>
2022-10-01 17:11:46 +0300

    задача про вариации расположения цифр в числе

---
+++ доп.задача/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+void rec(string s2, string c)
+{
+string s1;
+int i;
+if (s2.Length == 0)
+Console.WriteLine(c);
+else
+{
+for (i = 0; i < s2.Length; i++)
+{
+c += s2[i];
+s1 = s2;
+s1 = s1.Remove(i, 1);
+rec(s1, c);
+c = c.Remove(c.Length - 1, 1);
+}
+}
+}
+
+
+int i;
+string? s, c, s2;
+s = Console.ReadLine();
+for(i = 0; i < s.Length; i++)
+{
+c = Convert.ToString(s[i]);
+s2 = Convert.ToString(s);
+s2 = s2.Remove(i, 1);
+rec(s2, c);
+}
